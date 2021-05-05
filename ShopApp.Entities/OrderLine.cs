d8cbd6d03fa19c77ac39204f46086ec5b5d11a99
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-05 07:27:58 +0300

    create OrderLine class for the line of an order

---
+++ ShopApp.Entities/OrderLine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities
+{
+    class OrderLine
+    {
+        /* this class represents the line(command, desire) in an order */
+    }
+}
