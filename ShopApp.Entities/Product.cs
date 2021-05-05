678c7ccee65780ee08fd011c7598018fcc3ee61d
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-05 06:50:29 +0300

    create Product entitity

---
+++ ShopApp.Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities
+{
+    class Product
+    {
+        /* this is the model that we define Product */
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public decimal Price { get; set; }
+        
+    }
+}
