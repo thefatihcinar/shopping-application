5e12e53320540b492a516ef53cab5fefb2668a63
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-05 08:45:03 +0300

    create category repository

---
+++ ShopApp.DataAccess/Abstract/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopApp.Entities;
+
+namespace ShopApp.DataAccess.Abstract
+{
+    public interface ICategoryRepository: IRepository<Category>
+    {
+
+    }
+}
