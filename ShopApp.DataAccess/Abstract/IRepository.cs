e7cd17553a1d24152174222c4e7d2a4cf28e9949
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-05 08:42:08 +0300

    create generic repository IRepository for orders, products and categories repos

---
+++ ShopApp.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ShopApp.Entities;
+
+namespace ShopApp.DataAccess.Abstract
+{
+    public interface IRepository<Type>
+    {
+        Type GetById(int id);
+        /* get entity based on given id */
+
+        Type GetOne(Expression<Func<Type, bool>> filter);
+        /* get the first entity with a lambda expression */
+
+        IQueryable<Type> GetAll(Expression<Func<Type, bool>> filter);
+        /* get all entities with a lambda expression */
+
+        void Create(Type entity);
+        /* create an entity */
+
+        void Delete(Type entity);
+        /* delete an entity */
+
+        void Update(Type entity);
+        /* update an entity */
+
+
+    }
+}
