90383c94f0d35211a0e7abab174f1c17a4526de2
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-05 06:51:37 +0300

    create Category entitity

---
+++ ShopApp.Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities
+{
+    class Category
+    {
+        /* this is the model that we define a category */
+        
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
