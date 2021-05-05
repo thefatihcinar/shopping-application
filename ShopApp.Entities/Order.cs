235de638a00795a8ee84bbcfb1a85f12616b0d63
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-05 07:26:44 +0300

    create Order class for orders

---
+++ ShopApp.Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities
+{
+    class Order
+    {
+        /* this class represents an order */
+    }
+}
