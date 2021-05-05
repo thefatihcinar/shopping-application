83682d62099381666130888a0ffd27a28e7f5df8
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-05 08:16:18 +0300

    develop interface for Product Repository

--- ShopApp.DataAccess/Abstract/IProductRepository.cs
+++ ShopApp.DataAccess/Abstract/IProductRepository.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using ShopApp.Entities;
 
 namespace ShopApp.DataAccess.Abstract
 {
     public interface IProductRepository
     {
+        Product GetById(int id);
+        /* get product based on given id */
+
+        Product GetOne(Expression<Func<Product, bool>> filter);
+        /* get the first product with a lambda expression */
+
+        IQueryable<Product> GetAll(Expression<Func<Product, bool>> filter);
+        /* get all products with a lambda expression */
+
+        void Create(Product entity);
+        /* create a product */
+
+        void Delete(Product entity);
+        /* delete a product */
+
+        void Update(Product entity);
+        /* update a product */
+
 
     }
 }
