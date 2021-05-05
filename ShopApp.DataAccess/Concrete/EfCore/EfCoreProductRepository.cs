34a710c8cb01ac2ea52ab978d3c81f182ce83f06
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-05 08:19:55 +0300

    create entity framework core Product repository

---
+++ ShopApp.DataAccess/Concrete/EfCore/EfCoreProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+    public class EfCoreProductRepository : IProductRepository
+    {
+        public void Create(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Product> GetAll(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetOne(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
