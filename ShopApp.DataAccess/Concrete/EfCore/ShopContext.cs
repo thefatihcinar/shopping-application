c6561afe7d65db9ee8dcf57d06683e46ae7cad4a
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-11 17:50:46 +0300

    create Database Context for ShopDB

---
+++ ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+    public class ShopContext : DbContext
+    {
+        /* Context for Shop Database*/
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // telling to use SQL Server
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=ShopDb; integrated security=true;");
+            // This is the CONNECTION STRING
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
