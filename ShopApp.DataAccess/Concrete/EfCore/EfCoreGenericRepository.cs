d729f6da17a93e002af60bbde25119a4a69a764d
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-11 18:45:28 +0300

    create Generic Concrete Repository, why because database operations are common among all repos

---
+++ ShopApp.DataAccess/Concrete/EfCore/EfCoreGenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+    /*
+     * TypeEntity: this repository is about which entity
+     * TypeContext: this is the type of the DB Context
+     */
+    public class EfCoreGenericRepository<TypeEntity, TypeContext> : IRepository<TypeEntity>
+        where TypeEntity : class
+        where TypeContext : DbContext, new()
+    {
+        public void Create(TypeEntity entity)
+        {
+            using (var context = new TypeContext())
+            {
+                context.Set<TypeEntity>().Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TypeEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TypeEntity> GetAll(Expression<Func<TypeEntity, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeEntity GetOne(Expression<Func<TypeEntity, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TypeEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
