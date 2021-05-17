4da4064868f5640e75c892debeb2f4f9d79f6c0f
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-17 14:30:51 +0300

    create product service interface in business layer

---
+++ ShopApp.Business/Abstract/IProductService.cs
@@ -0,0 +1,27 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Abstract
+{
+    public interface IProductService
+    {
+        /*
+         * this is the services that product service offers
+         * which operations can be done
+         */
+        List<Product> GetAll();
+
+        Product GetById();
+
+        void Create(Product entity);
+
+        void Update(Product entity);
+
+        void Delete(Product entity);
+
+    }
+}
