7d32e5f744c01e41b45aa3d4ad34851bc959db0e
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-06-03 14:06:57 +0300

    create admin controller for admin pages

---
+++ ShopApp.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        /* this is for admin pages */
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
