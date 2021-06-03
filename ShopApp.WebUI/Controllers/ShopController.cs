7724d399590c8a70723a75f3973364612e95a299
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-06-03 14:07:24 +0300

    create shop controller for user pages

---
+++ ShopApp.WebUI/Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class ShopController : Controller
+    {
+        /* shop controller is for user pages */
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
