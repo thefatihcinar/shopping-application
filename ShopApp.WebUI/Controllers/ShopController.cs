a8deb3d2c8663fa651451c3525d1e011d69cbcae
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-06-04 14:16:28 +0300

    added Details action for shop controller, which shows details of a product

--- ShopApp.WebUI/Controllers/ShopController.cs
+++ ShopApp.WebUI/Controllers/ShopController.cs
@@ -13,6 +13,7 @@ namespace ShopApp.WebUI.Controllers
         /* shop controller is for user pages */
 
         private IProductService _productService;
+
         public ShopController(IProductService productService)
         {
             _productService = productService;
@@ -29,11 +30,36 @@ namespace ShopApp.WebUI.Controllers
             /* this controller is for listing all the products in the app */
 
             /* return all the products to the rendering */
-            var allTheProducts = new ProductListModel() {
+            var allTheProducts = new ProductListModel()
+            {
                 Products = _productService.GetAll()
             };
 
             return View(allTheProducts);
+        }
+
+        public IActionResult Details(int? ID)
+        {
+            /* this controller will show details of a product
+               ürünlerin detaylarını gösterir, ürün sayfası
+             */
+
+            if (ID == null)
+            {
+                // if there is no id data given, return not found
+                return NotFound();
+            }
+            // else go fetch the product with the given id
+            var theProduct = _productService.GetById((int)ID);
+            // there might be NO PRODUCT with the given id
+            if (theProduct == null)
+            {
+                // there is no such product
+                return NotFound();
+            }
+
+            // in this case, product found and valid
+            return View(theProduct);
         }
     }
 }
