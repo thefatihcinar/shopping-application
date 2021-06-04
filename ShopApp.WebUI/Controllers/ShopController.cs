766c192692eff576cb17b3ff37b44e3a809feae9
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-06-04 10:04:14 +0300

    create List method in Shop controller, for user products

--- ShopApp.WebUI/Controllers/ShopController.cs
+++ ShopApp.WebUI/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.Business.Abstract;
+using ShopApp.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,29 @@ namespace ShopApp.WebUI.Controllers
     public class ShopController : Controller
     {
         /* shop controller is for user pages */
+
+        private IProductService _productService;
+        public ShopController(IProductService productService)
+        {
+            _productService = productService;
+            // dependency injection
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult List()
+        {
+            /* this controller is for listing all the products in the app */
+
+            /* return all the products to the rendering */
+            var allTheProducts = new ProductListModel() {
+                Products = _productService.GetAll()
+            };
+
+            return View(allTheProducts);
         }
     }
 }
