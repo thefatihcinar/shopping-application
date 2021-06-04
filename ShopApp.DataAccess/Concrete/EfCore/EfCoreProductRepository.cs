cb00b3ccd9a607f95109310f27afd3a31c7cdd01
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-06-04 20:04:36 +0300

    added GetProductDetails, get categories of a product using sql and ef core commands in concrete repo

--- ShopApp.DataAccess/Concrete/EfCore/EfCoreProductRepository.cs
+++ ShopApp.DataAccess/Concrete/EfCore/EfCoreProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using ShopApp.DataAccess.Abstract;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.DataAccess.Abstract;
 using ShopApp.Entities;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,20 @@ namespace ShopApp.DataAccess.Concrete.EfCore
 {
     public class EfCoreProductRepository : EfCoreGenericRepository<Product, ShopContext>, IProductRepository
     {
-       
+        public Product GetProductDetails(int id)
+        {
+            /* this method will return details of a product
+             especially category information embedded using SQL commands */
+
+            using (var context = new ShopContext())
+            {
+                // connect to the database
+                return context.Products
+                              .Where(product => product.Id == id) // filter rows by id
+                              .Include(reach => reach.ProductCategories) // join products table with productscategories table
+                              .ThenInclude(reach => reach.Category)
+                              .FirstOrDefault();
+            }
+        }
     }
 }
