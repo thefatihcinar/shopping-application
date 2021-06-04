129bba80575d27d683c01c1bca6998a5108791d3
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-06-04 20:05:26 +0300

    create new ViewModel for displaying categories of a product

---
+++ ShopApp.WebUI/Models/ProductDetailsModel.cs
@@ -0,0 +1,17 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Models
+{
+    public class ProductDetailsModel
+    {
+        /* this model will be used to render products and categories to the screen */
+
+        public Product Product { get; set; }
+
+        public List<Category> Categories { get; set; }
+    }
+}
