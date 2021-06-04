37317957cda1f09ec3847b14a4f8e95bd45f92fe
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-06-04 13:39:03 +0300

    add Description property to Product entity

--- ShopApp.Entities/Product.cs
+++ ShopApp.Entities/Product.cs
@@ -11,11 +11,12 @@ namespace ShopApp.Entities
         /* this is the model that we define Product */
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public string Description { get; set; }
         public string ImageURL { get; set; }
         public decimal Price { get; set; }
 
         public List<ProductCategory> ProductCategories { get; set; }
         /* this products belongs to which categories */
-        
     }
 }
