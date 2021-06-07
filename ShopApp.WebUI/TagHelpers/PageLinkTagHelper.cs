c4d415299765c8b7d9234b0dff696d08dc96fad4
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-06-07 15:07:17 +0300

    create page link tag helper class, that generates pagination html code

---
+++ ShopApp.WebUI/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using ShopApp.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        public PageInfo PageModel { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("<ul class='pagination'>");
+
+            for (int page = 1; page <= PageModel.TotalPages(); page++)
+            {
+                stringBuilder.AppendFormat("<li class='page-item {0}'>", page == PageModel.CurrentPage ? "active" : "");
+
+                if (string.IsNullOrEmpty(PageModel.CurrentCategory))
+                {
+                    stringBuilder.AppendFormat("<a class='page-link' href='/products?page={0}'>{0}</a>", page);
+                }
+                else
+                {
+                    stringBuilder.AppendFormat("<a class='page-link' href='/products/{0}?page={1}'>{1}</a>", PageModel.CurrentCategory, page);
+                }
+
+                stringBuilder.Append("</li>");
+            }
+
+            output.Content.SetHtmlContent(stringBuilder.ToString());
+
+            base.Process(context, output);
+        }
+    }
+}
