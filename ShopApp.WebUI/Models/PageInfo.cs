d583b32049802c6fe6b1a573f7d9d5abd3fec3a0
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-06-07 15:08:11 +0300

    create PageInfo model for bringin pagination information

---
+++ ShopApp.WebUI/Models/PageInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Models
+{
+    public class PageInfo
+    {
+        public int TotalItems { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public string CurrentCategory { get; set; }
+
+        public int TotalPages()
+        {
+            return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+        }
+    }
+}
