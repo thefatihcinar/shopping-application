dba274626c1ade07a9f2c795c0e3f7a6a627dd06
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-06-08 14:05:58 +0300

    create Product Model for reading and inputting data from form

---
+++ ShopApp.WebUI/ViewModels/ProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.ViewModels
+{
+    public class ProductModel
+    {
+        /* this is a ViewModel for Product entity */
+        /* generally for front-end operations */
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public decimal Price { get; set; }
+    }
+}
