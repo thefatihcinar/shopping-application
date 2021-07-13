95388e36ebed5d4d0a20c0daf649366d6567bf48
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-07-13 16:05:52 +0300

    [change dir and rename] Product List View Model

--- ShopApp.WebUI/ViewModels/ProductListViewModel.cs
+++ ShopApp.WebUI/ViewModels/ProductListViewModel.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ShopApp.WebUI.Models
+namespace ShopApp.WebUI.ViewModels
 {
-    public class ProductListModel
+    public class ProductListViewModel
     {
         public PageInfo PaginationInformation { get; set; }
         public List<Product> Products { get; set; }
