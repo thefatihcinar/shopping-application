d97c876ae3247f58bb916e33e3bbf80c193b7835
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-07-13 12:38:19 +0300

    AutoMapper configuration files

---
+++ ShopApp.WebUI/AutoMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ShopApp.Entities;
+using ShopApp.WebUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI
+{
+    public class AutoMapping: Profile
+    {
+        public AutoMapping()
+        {
+            CreateMap<ProductViewModel, Product>(); // ProductVM -> Product
+        }
+    }
+}
