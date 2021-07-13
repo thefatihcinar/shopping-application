b8f65638cca600336465a181658a20876b97115a
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-07-13 16:22:29 +0300

    [bug fix] re-added Error View Model

---
+++ ShopApp.WebUI/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopApp.WebUI.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
