9333b2cea03ded8d9b45f7e7c320f771b94989cb
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-07-13 16:05:13 +0300

    [change dir and rename] category list view model

--- ShopApp.WebUI/ViewModels/CategoryListViewModel.cs
+++ ShopApp.WebUI/ViewModels/CategoryListViewModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ShopApp.WebUI.Models
+namespace ShopApp.WebUI.ViewModels
 {
     public class CategoryListViewModel
     {
