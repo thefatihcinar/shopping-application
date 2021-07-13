ed8768509ee83653a45003e68e963cf16f66a738
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-07-13 16:07:02 +0300

    [change directory] page info

--- ShopApp.WebUI/ViewModels/PageInfo.cs
+++ ShopApp.WebUI/ViewModels/PageInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ShopApp.WebUI.Models
+namespace ShopApp.WebUI.ViewModels
 {
     public class PageInfo
     {
