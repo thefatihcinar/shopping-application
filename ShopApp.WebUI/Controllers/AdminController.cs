4c13080b5c369e86cf49d8134fe6f95ed95e863a
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-07-14 15:48:23 +0300

    add edit controller

--- ShopApp.WebUI/Controllers/AdminController.cs
+++ ShopApp.WebUI/Controllers/AdminController.cs
@@ -49,5 +49,51 @@ namespace ShopApp.WebUI.Controllers
             return RedirectToAction("Index");
 
         }
+
+        [HttpGet]
+        public IActionResult EditProduct(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            // get the product
+            var product = _productService.GetById((int) id);
+
+            // there might be no product with this id
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            // create view model for product from product
+            var productViewModel = _mapper.Map<ProductViewModel>(product);
+            
+            // render this product to the screen for editing
+            return View(productViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult EditProduct(ProductViewModel model)
+        {
+            // Go get the product with this given id
+            // we have made the id attribute hidden in the form
+            Product theProduct = _productService.GetById(model.Id);
+
+            // the entity might be null, or it might not exist
+            if(theProduct == null)
+            {
+                return NotFound();
+            }
+
+            // Update this product, using the view model
+            // First convert the view model to an actual model
+            theProduct = _mapper.Map<Product>(model);
+
+            _productService.Update(theProduct);
+
+            return RedirectToAction("index");
+        }
     }
 }
