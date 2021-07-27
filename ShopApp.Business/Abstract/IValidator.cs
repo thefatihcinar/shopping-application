73454fe0f088db3eb329209481d3849df10e6307
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-07-27 13:54:09 +0300

    create Validator Interface

---
+++ ShopApp.Business/Abstract/IValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Abstract
+{
+    public interface IValidator<Type>
+    {
+        Dictionary<string, string> Error { get; set; }
+        // This dictionary will contain all error messages
+
+        bool Validate(Type entity);
+        // This method will validate an entity
+    }
+}
